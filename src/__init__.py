7860806aae09b192dbddb4e8d53c64f14eed3bec
schizoaidev <schizoaidev@proton.me>
2024-12-20 09:00:24 +0000

    Change interval

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,8 @@ load_dotenv()
 from ai import get_ai_response
 from twitter import send_tweet
 import time
+import random
 
 while (True):
     send_tweet(get_ai_response())
-    time.sleep(300)
+    time.sleep(random.randint(120, 600))
